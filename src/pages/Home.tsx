aa851ecb9ebddb66d3f7b558a6f847b48107f80b
Edita Motuze <edita.motuze@gmail.com>
2024-08-05 16:30:37 +0300

    feat: created images list section

--- src/pages/Home.tsx
+++
@@ -1,7 +0,0 @@
-import Main from "../components/Main/Main";
-
-const Home: React.FC = () => {
-  return <Main>Home</Main>;
-};
-
-export default Home;
---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import Main from "../../components/Main/Main";
+import ImagesList from "./sections/ImagesList/ImagesList";
+
+const Home: React.FC = () => {
+  return (
+    <Main>
+      <ImagesList />
+    </Main>
+  );
+};
+
+export default Home;
