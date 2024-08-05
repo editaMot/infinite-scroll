f73719618a804233349044ba4eb509e163d01f53
Edita Motuze <edita.motuze@gmail.com>
2024-08-05 15:41:38 +0300

    feat: created component for main

---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,12 @@
+import { ReactNode } from "react";
+import styles from "./Main.module.scss";
+
+interface MainProps {
+  children: ReactNode;
+}
+
+const Main: React.FC<MainProps> = ({ children }) => {
+  return <main className={styles.main}>{children}</main>;
+};
+
+export default Main;
